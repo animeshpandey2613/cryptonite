a05e91139f307249701be8b752e719846af2faea
animeshpandey2613 <121745585+animeshpandey2613@users.noreply.github.com>
2024-07-21 14:17:59 +0530

    home issue fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,38 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
-export default function Home() {
+import React, { useEffect, useState } from "react";
+import Chart from "./component/Home/chart";
+import Table from "./component/Home/Table";
+import Form from "./component/Home/ResetPassword2/Form";
+import style from "./page.module.css";
+import Blocks from "./component/Home/Blocks";
+import Navbar from "./component/Navbar/Navbar";
+import Footer from "./component/Footer/Footer";
+const HomePage = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 7000);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
-    <main>home</main>
-  )
-}
+    <main>
+      <Form />
+      <Navbar />
+      {!loading && (
+        <>
+          <div className={style.mainArea}>
+            <div className={style.leftArea}>
+              <Chart />
+              <Table />
+            </div>
+            <Blocks />
+          </div>
+          <Footer />
+        </>
+      )}
+    </main>
+  );
+};
+
+export default HomePage;
